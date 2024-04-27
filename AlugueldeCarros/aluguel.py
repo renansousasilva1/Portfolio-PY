f00ec34bf1f65248a9b0b24237c2d8ec88bbd9ad
renansousasilva1 <84554881+renansousasilva1@users.noreply.github.com>
2024-04-27 20:49:05 -0300

    Delete AlugueldeCarros directory

--- AlugueldeCarros/aluguel.py
+++
@@ -1,8 +0,0 @@
-aluguel = int(input("Por quantos dias o carro foi alugado? "))
-km= float(input("Quantos km rodados? "))
-precodia = aluguel*60
-precokm = km*0.15
-precofinal = precodia + precokm
-
-print("O Total a pagar é de R$", precofinal)
-
