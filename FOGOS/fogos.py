2fdc60d36829c393601921a9f50658bc865c1274
renansousasilva1 <84554881+renansousasilva1@users.noreply.github.com>
2024-04-30 10:27:55 -0300

    Contagem Regressiva
    Este código mostra uma simples contagem regressiva e exibe alguma mensagem final

---
+++ FOGOS/fogos.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+for c in range (0, 11):
+    sleep(1)
+    print(c)
+emoji_fireworks = "\U0001F386"
+print("Acabou! Feliz Ano Novo!")
+print(emoji_fireworks, emoji_fireworks, "Queima de fogos", emoji_fireworks, emoji_fireworks)
+    
